9db5926ab45f87cd02ca3dc2e7ba1af0cf14549b
PrtagonistOne <playerdna4@gmail.com>
2022-08-12 18:30:21 +0300

    Continuous movement left and right

--- character.py
+++ character.py
@@ -1,36 +1,25 @@
 import pygame
+from settings import Settings
 
 
-class Ship:
+class Character:
     """A class to manage the ship"""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = Settings()
 
-        # Movement Flags
-        self.moving_right = False
-        self.moving_left = False
         # Load the ship image and get its rect.
         self.image = pygame.transform.smoothscale(
-            pygame.image.load('images/spaceship.bmp'), (70, 100))
+            pygame.image.load('images/alien_character.bmp'), (70, 100))
 
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom
-
-
-    def update(self):
-        """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        self.rect.midbottom = self.screen_rect.center
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
-
--- settings.py
+++ settings.py
@@ -6,4 +6,8 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 200)
+        self.bg_color = (0, 0, 139)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
