8ef2e880dc40bb7f0ec3c0d4b4a4d976764f4cd7
PrtagonistOne <playerdna4@gmail.com>
2022-08-21 15:43:08 +0300

    Add menu buttons

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ class Settings:
         self.bg_color = (0, 0, 139)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2
         self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 0 ,0)
         self.bullets_allowed = 3
@@ -23,7 +23,7 @@ class Settings:
         self.rocket_speed = 1.3
 
         # Alien settings
-        self.alien_speed = 10.0
+        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
